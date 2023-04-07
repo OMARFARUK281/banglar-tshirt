b8a62c9ff041275faf8fd444764d51d8decf8141
OmarFaruk <omarfaruk8431@gmail.com>
2023-04-07 10:15:13 +0600

    added grandpa and uncle money

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import './Grandpa.css'
-import Father from '../Father/Father';
-import Uncle from '../Uncle/Uncle';
-import Aunty from '../Aunty/Aunty';
+import React, { createContext, useState } from "react";
+import "./Grandpa.css";
+import Father from "../Father/Father";
+import Uncle from "../Uncle/Uncle";
+import Aunty from "../Aunty/Aunty";
+
+export const RingContext = createContext("gold");
+export const MoneyContext = createContext(0);
 
 const Grandpa = () => {
-    const ring = 'diamond';
+  const ring = "diamond";
+  const [money, setMoney] = useState(0);
 
-    return (
-        <div className='grandpa'>
-            <h2>Grandpa</h2>
-            <section className='flex'>
-                <Father ring={ring}></Father>
-                <Uncle></Uncle>
-                <Aunty ring={ring}></Aunty>
-            </section>
-        </div>
-    );
+  return (
+    <div className="grandpa">
+      <h2>Grandpa</h2>
+      <p>Has Money: {money}</p>
+      <MoneyContext.Provider value={[money, setMoney]}>
+        <RingContext.Provider value="golden Ring">
+          <section className="flex">
+            <Father ring={ring}></Father>
+            <Uncle></Uncle>
+            <Aunty ring={ring}></Aunty>
+          </section>
+        </RingContext.Provider>
+      </MoneyContext.Provider>
+    </div>
+  );
 };
 
 export default Grandpa;
+
+/*
+1. create context and export
+2. create a provider and pass a value
+3. use useContext to receive
+ */
